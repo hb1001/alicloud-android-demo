fb6fb5f55571509e207a4023139019d41bb0d873
zhanghaibo <1941670713@qq.com>
2025-10-01 00:42:36 +0800

    修改了java代码，资源文件，上传到网站了， 155727

--- hotfix_android_demo/src/main/java/com/alibaba/sophix/demo/DexFixDemo.java
+++ hotfix_android_demo/src/main/java/com/alibaba/sophix/demo/DexFixDemo.java
@@ -33,8 +33,8 @@ public class DexFixDemo {
     }
 
     public static void test_normal(Context context) {
-        Toast.makeText(context, "old apk from java...", Toast.LENGTH_SHORT).show();
-//        Toast.makeText(context, "new apk from java...", Toast.LENGTH_SHORT).show();
+//        Toast.makeText(context, "old apk from java...", Toast.LENGTH_SHORT).show();
+        Toast.makeText(context, "new apk from java...", Toast.LENGTH_SHORT).show();
     }
 
 
@@ -42,14 +42,16 @@ public class DexFixDemo {
         A obj = new A();
         StringBuilder stringBuilder = new StringBuilder("test_addField")
                 .append(" a:").append(obj.a)
-                //.append(" b:").append(obj.b)
-                .append(" c:").append(obj.c);
+                .append(" b:").append(obj.b) // <-- 同时把 b 的访问也打开
+                .append(" c:").append(obj.c)
+                .append(" d:").append(obj.d); // <-- 访问新增的成员变量
         Log.d("Sophix", stringBuilder.toString());
     }
 
     public static void test_addMethod() {
         A aObj = new A();
         aObj.a_t2();
+        aObj.a_t3(); // <-- 调用新增的方法
     }
 }
 
@@ -57,12 +59,17 @@ class A {
     int a = 0;
     int b = 1;
     int c = 2;
+    int d = 3; // <-- 新增的成员变量
 
     void a_t1() {
         Log.d("Sophix", "A a_t1");
     }
     void a_t2() {
         Log.d("Sophix", "A a_t2");
+    }
+
+    void a_t3() { // <-- 新增的方法
+        Log.d("Sophix", "A a_t3 invoke success!");
     }
 }
 
